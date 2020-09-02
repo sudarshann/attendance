20015f0c92e8bd173a07b156596a1f3c1194747c
amalrajsebastin <amalrajkarthick@gmail.com>
2020-09-02 19:12:01 +0530

    folder structure
    plugin assets structure change

--- Attendance/Attendance.php
+++ Attendance/Attendance.php
@@ -6,67 +6,63 @@
    Version: 1.0
    Author:  Amal
    */
+
+
    class Attendance
    {
 
       public static  function start()
       {
-            add_action("init",array(get_called_class(),"activate"));
-            add_action("init",array(get_called_class(),"deactivate"));
-            add_action("init",array(get_called_class(),"uninstall"));
-             // add_action('wp_enqueue_style',array(get_called_class(),'assets'));
+            // add_action( 'init',array(get_called_class(),'activate'));
+            // add_action( 'init',array(get_called_class(),'deactivate'));
+            // add_action( 'init',array(get_called_class(),'uninstall'));
             add_action("admin_menu",array(get_called_class(),"custom_admin_menu"));
             add_action('admin_enqueue_scripts',array(get_called_class(),'enque_bootstrap_library'));
             add_action('admin_enqueue_scripts',array(get_called_class(),'enque_calander_library'));
             add_action('admin_enqueue_scripts',array(get_called_class(),'enque_multiselect_library'));
             add_action('admin_enqueue_scripts',array(get_called_class(),'enque_custom_library'));
-            add_action( 'init',array(get_called_class(),'ajax_script_enqueuer'));
-            add_action( 'init',array(get_called_class(),'ajax_script_enqueuer'));
-            add_action( 'init',array(get_called_class(),'ajax_script_enqueuer'));
             add_action("wp_ajax_create_checkin",array(get_called_class(), "create_checkin"));
             add_action("wp_ajax_get_checkin_list",array(get_called_class(), "get_checkin_list"));
             add_action("wp_ajax_delete_checkin",array(get_called_class(), "delete_checkin"));
             add_action("init",array(get_called_class(), "edit_submit"));
-            // add_action("wp_ajax_nopriv_my_user_vote", "my_must_login");
 
       }
-      public static function activate()
-      {
-         flush_rewrite_rules();
-      }
-      public static function deactivate()
-      {
-         flush_rewrite_rules();
-      }
-      public static function uninstall()
-      {
+   public static function activate()
+   {
+      flush_rewrite_rules();
+   }
+   public static function deactivate()
+   {
+      flush_rewrite_rules();
+   }
+   public static function uninstall()
+   {
 
-      }
+   }
 
    public static function custom_admin_menu ()
    {
          add_menu_page('Attendence', 'Attendence', 'manage_options', 'attendence-slug',array( get_called_class(), 'content'));
-         add_submenu_page('attendence-slug', 'Attendence', 'Attendence', 'manage_options', 'attendence-slug', array( get_called_class(), 'content'));
+         // add_submenu_page('attendence-slug', 'Attendence', 'Attendence', 'manage_options', 'attendence-slug', array( get_called_class(), 'content'));
    }
 
    public static function ajax_script_enqueuer() {
-      wp_register_script( "ajax_attendance_script", get_template_directory_uri().'/classes/Scit/Attendence/script.js', array('jquery') );
+      wp_register_script( "ajax_attendance_script",plugin_dir_url(__FILE__).'/assets/css/script.js', array('jquery') );
       wp_localize_script( 'ajax_attendance_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
       wp_enqueue_script( 'jquery' );
       wp_enqueue_script( 'ajax_attendance_script' );
-
    }
 
    public static function enque_bootstrap_library(  ) {
 
-     wp_enqueue_style('bootstrapstyle',get_template_directory_uri().'/assets/bootstrap/css/bootstrap.min.css');
-     wp_enqueue_script('bootstrapjs',get_template_directory_uri().'/assets/bootstrap/js/bootstrap.min.js');
+     wp_enqueue_style('bootstrap',plugin_dir_url(__FILE__).'/assets/css/bootstrap.min.css');
+     wp_enqueue_script('bootstrap',plugin_dir_url(__FILE__).'/assets/js/bootstrap.min.js');
 
-     wp_enqueue_script('datatable',get_template_directory_uri().'/classes/Scit/Attendence/DataTables/datatables.js');
-     wp_enqueue_style('datatable',get_template_directory_uri().'/classes/Scit/Attendence/DataTables/datatables.css');
+     wp_enqueue_script('datatable',plugin_dir_url(__FILE__).'/assets/js/datatables.min.js');
+     wp_enqueue_style('datatable',plugin_dir_url(__FILE__).'/assets/css/datatables.min.css');
 
-     wp_enqueue_script('datatable',get_template_directory_uri().'/classes/Scit/Attendence/DataTables/Buttons/js/buttons.bootstrap.js');
-     wp_enqueue_style('datatable',get_template_directory_uri().'/classes/Scit/Attendence/DataTables/Buttons/js/buttons.bootstrap.css');
+     wp_enqueue_script('datatable_button',plugin_dir_url(__FILE__).'/assets/js/buttons.bootstrap.min.js');
+     wp_enqueue_style('datatable_button',plugin_dir_url(__FILE__).'/assets/css/buttons.bootstrap.min.css');
       
      wp_enqueue_style('jqueryui','https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
 
@@ -75,23 +71,23 @@
 
    public static function enque_calander_library(  ) {
       
-      wp_enqueue_style('calnderjs',get_template_directory_uri().'/classes/Scit/Attendence/Calander/lib/main.css');
-      wp_enqueue_script('calandercss',get_template_directory_uri().'/classes/Scit/Attendence/Calander/lib/main.js');
+      wp_enqueue_style('calnderjs',plugin_dir_url(__FILE__).'/assets/css/calander.min.css');
+      wp_enqueue_script('calandercss',plugin_dir_url(__FILE__).'/assets/js/calander.min.js');
 
    }
 
    public static function enque_multiselect_library()
    {
-      wp_enqueue_script('timepickerjs',get_template_directory_uri().'/classes/Scit/Attendence/timepicker/src/jquery.ptTimeSelect.js');
-      wp_enqueue_style('timepickercss',get_template_directory_uri().'/classes/Scit/Attendence/timepicker/src/jquery.ptTimeSelect.css');
+      wp_enqueue_script('timepickerjs',plugin_dir_url(__FILE__).'assets/js/jquery.ptTimeSelect.js');
+      wp_enqueue_style('timepickercss',plugin_dir_url(__FILE__).'assets/css/jquery.ptTimeSelect.css');
 
-      wp_enqueue_style('multiselectcss',get_template_directory_uri().'/classes/Scit/Attendence/MultiSelect/jquery.multiselect.css');
-      wp_enqueue_script('multiselectjs',get_template_directory_uri().'/classes/Scit/Attendence/MultiSelect/jquery.multiselect.js');
+      wp_enqueue_style('multiselectcss',plugin_dir_url(__FILE__).'assets/css/jquery.multiselect.css');
+      wp_enqueue_script('multiselectjs',plugin_dir_url(__FILE__).'assets/js/jquery.multiselect.js');
    }
 
    public static function enque_custom_library()
    {
-     wp_enqueue_style('customcss',get_template_directory_uri().'/classes/Scit/Attendence/style.css');
+     wp_enqueue_style('customcss',plugin_dir_url(__FILE__).'/assets/css/style.css');
    }
    
    public static function content()
